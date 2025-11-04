72a688713050638a8124dae9834b54487218ad60
Gajendra L <100460060+Gajaindra@users.noreply.github.com>
2025-11-04 15:27:09 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    return 0; // TODO: Students implement
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", factorial(n));
+    return 0;
+}
